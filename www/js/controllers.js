25cf5bd569f2c6232149fe2bf4036f8bbbfbe8a0
田杰 <tianjie@cncms.com>
2015-08-04 20:20:12 +0800

    Bind weibo.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,46 +1,48 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', ['Recover', 'Weibo', function(Recover, Weibo) {
-  eval(Recover.get());
-}])
+    .controller('AppCtrl', ['Recover', 'Weibo', function (Recover, Weibo) {
+        eval(Recover.get());
+    }])
 
-.controller('DashCtrl', [function($scope) {}])
+    .controller('DashCtrl', [function ($scope) {
+    }])
 
-.controller('ChatsCtrl', ['$scope', 'Chats', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    .controller('ChatsCtrl', ['$scope', 'Chats', function ($scope, Chats) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-}])
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        };
+    }])
 
-.controller('ChatDetailCtrl', ['$scope', '$stateParams', 'Chats', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-}])
+    .controller('ChatDetailCtrl', ['$scope', '$stateParams', 'Chats', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    }])
 
-.controller('AccountCtrl', ['$scope', 'Weibo', function($scope, Weibo) {
-  $scope.settings = {
-    bindWeibo: false
-  };
+    .controller('AccountCtrl', ['$scope', 'Weibo', '$timeout', '$interval', function ($scope, Weibo, $timeout, $interval) {
+        $scope.settings = {
+            bindWeibo: Weibo.hasBound()
+        };
 
-  $scope.bindWeibo = function() {
-    Weibo.authorize(function(code) {
-      alert(code);
-    }, function(message, reason) {
-      alert(message);
-    });
-  };
+        $interval(function () {
+            $scope.settings.bindWeibo = Weibo.hasBound();
+        }, 5000);
 
-  $scope.$watch('settings.bindWeibo', function(newValue, oldValue) {
-    if (!oldValue && newValue) {
-      Weibo.bind();
-    }
-  });
-}]);
+        $scope.$watch('settings.bindWeibo', function (newValue, oldValue) {
+            if (!oldValue && newValue) {
+                Weibo.bind()
+                    .then(null, function () {
+                        $timeout(function () {
+                            $scope.settings.bindWeibo = false;
+                        })
+                    });
+            }
+        });
+    }]);
