c687e767d3bb291460b33c2be877dd0274829898
田杰 <jefftian@hcdlearning.com>
2015-09-04 11:26:03 +0800

    change proxy to https.

--- www/js/services.js
+++ www/js/services.js
@@ -311,7 +311,7 @@ angular.module('starter.services', [])
 
     .service('Proxy', ['DeviceHelper', function (DeviceHelper) {
         this.proxyNative = function (url) {
-            return '{0}?url={1}'.format('http://meiyanruhua.tao3w.com/proxy.php', encodeURIComponent(url)) + '&mode=native&send_cookies=true';
+            return '{0}?url={1}'.format('https://proxy-php.herokuapp.com/proxy2.php', encodeURIComponent(url)) + '&mode=native&send_cookies=true';
         };
 
         var self = this;
