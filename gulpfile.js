f16cfd2c46aee915cdda7bf469903d5d2b26e786
田杰 <jefftian@hcdlearning.com>
2016-03-14 17:45:05 +0800

    Add mocha to dependency.

--- gulpfile.js
+++ gulpfile.js
@@ -204,7 +204,7 @@ gulp.task('mock-release', function (done) {
 
 gulp.task('local-release', ['mock-release', 'default']);
 
-gulp.task('release', ['mock-release', 'jshint', 'mocha', 'test', 'start']);
+gulp.task('release', ['mock-release', 'jshint', 'mocha', 'test']);
 
 gulp.task('mocha', function (done) {
     sh.exec('mocha', done);
