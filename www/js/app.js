61a15b31322ed757cfb9732d64d30074709835df
Jeff Tian <jeff.tian@outlook.com>
2019-10-13 20:55:13 +0800

    feat: sign

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-    .run(['$ionicPlatform', '$rootScope', 'ChatCourier', function ($ionicPlatform, $rootScope, ChatCourier) {
+    .run(['$ionicPlatform', '$rootScope', 'ChatCourier', '$http', function ($ionicPlatform, $rootScope, ChatCourier, $http) {
         $ionicPlatform.ready(function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
@@ -23,6 +23,22 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         });
 
         $rootScope.ChatCourier = ChatCourier;
+
+        $rootScope.$on('$stateChangeStart',
+            function (event, toState, toParams, fromState, fromParams, options) {
+                $http
+                    .get('https://uniheart.pa-ca.me/wechat-dev/js-sdk-sign?select=wechat').then(res => {
+                        console2.log('res = ', res)
+                        wx.config({
+                            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                            appId: res.data.appId, // 必填，公众号的唯一标识
+                            timestamp: res.data.timestamp, // 必填，生成签名的时间戳
+                            nonceStr: res.data.nonceStr, // 必填，生成签名的随机串
+                            signature: res.data.signature, // 必填，签名
+                            jsApiList: [] // 必填，需要使用的JS接口列表
+                        });
+                    })
+            })
     }])
 
     .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
@@ -83,5 +99,4 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
         // if none of the above states are matched, use this as the fallback
         $urlRouterProvider.otherwise('/tab/dash');
-
     }]);
