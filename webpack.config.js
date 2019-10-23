0ef8b887caf4951b8319b2f6659f20968afb4c84
Jeff Tian <jie.tian@unicareer.com>
2019-10-23 13:23:41 +0800

    feat: handle images

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,24 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'from/main.js'
+        path: path.resolve(__dirname, 'dist/from'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
     }
 };
