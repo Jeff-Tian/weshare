c601f3be4b7eb2953075bb9df997b6f8779a0b63
Jeff Tian <jie.tian@unicareer.com>
2019-10-23 13:44:50 +0800

    refactor: encapsulate to components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from '../resources/icon.png';
-import 'victormono'
-
-function component() {
-    const element = document.createElement('div');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    return element;
-}
+import component from './components/webpack-assets-management-test'
 
 document.body.appendChild(component());
