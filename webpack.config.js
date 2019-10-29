992ae574e4f0ca1a937080bf210a19a4a22a1db4
Jeff Tian <jie.tian@unicareer.com>
2019-10-29 12:51:03 +0800

    fix: public path

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist/from'),
         filename: '[name].[hash].js',
-        publicPath: '/'
+        publicPath: process.env.NODE_ENV === 'production' ? '/from' : '/'
     },
     optimization: {
         moduleIds: 'hashed',
