88519ad7a9d19ff1468953c5b0ad3148061ca683
Jeff Tian <jie.tian@unicareer.com>
2019-11-01 17:59:12 +0800

    feat: location.search

--- src/components/share/form.tsx
+++ src/components/share/form.tsx
@@ -107,7 +107,7 @@ class ShareForm extends React.Component<ShareFormProps> {
             this.setState({qrCode: await toDataURL(this.state.shareLink)})
         });
 
-        fetch('https://uniheart.pa-ca.me/wechat-dev/js-sdk-sign?select=wechat&url=' + encodeURIComponent(location.origin + location.pathname)).then(r => r.json()).then((json: any) => {
+        fetch('https://uniheart.pa-ca.me/wechat-dev/js-sdk-sign?select=wechat&url=' + encodeURIComponent(location.origin + location.pathname + location.search)).then(r => r.json()).then((json: any) => {
             wx.config({
                 debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: json.appId, // 必填，公众号的唯一标识
